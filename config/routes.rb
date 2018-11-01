b3f6a4effa870073ef2e838ec6f2acdb30f6d7ca
Apostolis Stergiou <apostergiou@gmail.com>
2018-11-01 08:57:55 +0100

    fix quotes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
   get 'signup', to: 'users#new', as: 'signup'
-  get 'login', to: 'sessions#new’, as: 'login'
+  get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
 end
