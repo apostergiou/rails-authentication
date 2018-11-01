a129188c683251380dcfe1b54ce5bd51c02afe64
Apostolis Stergiou <apostergiou@gmail.com>
2018-11-01 08:53:28 +0100

    Implement sessions for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root 'home#index'
+
   resources :users
-  get 'home/index'
+  resources :sessions, only: [:new, :create, :destroy]
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new’, as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
 end
